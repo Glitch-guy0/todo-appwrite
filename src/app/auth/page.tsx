12e14b9c36cdebc021c47a68ec230d363b9e347c
Glitch-guy0 <prajwalm9845@gmail.com>
2024-11-30 21:20:38 +0530

    hydration bug solved
    it was a plugin problem, messing up the structure

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,42 +2,40 @@
 
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { GiConsoleController } from "react-icons/gi";
 
-export default function magicLogin() {
+export default function MagicLogin() {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    if (window !== undefined) {
+    if(typeof window !== "undefined") {
       const searchParams = new URLSearchParams(window.location.search);
       const userId = searchParams.get("userId");
       const secret = searchParams.get("secret");
       console.log(userId, secret);
-
       try {
-        axios.post("/api/session", { userId, secret });
+        if(userId && secret){
+          axios.post("/api/session", { userId, secret });
+        }
       } catch (err) {
         setError(true);
       }
     }
-  });
+  }, []); 
 
-  return (
-    <>
+  if (error) {
+    return (
       <div className="h-svh w-full flex justify-center items-center flex-col">
-        {error ? (
-          <>
-            <p className="text-center text-red-500">
-              Something went wrong, try again later
-            </p>
-          </>
-        ) : (
-          <>
-            <div className="m-auto my-2 rounded-full h-10 w-10 border-t-4 border-white/50 animate-spin"></div>
-            <p className="text-xl text-center">Redirecting ...</p>
-          </>
-        )}
+        <p className="text-center text-red-500">
+          Something went wrong, try again later
+        </p>
       </div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <div className="h-svh w-full flex justify-center items-center flex-col">
+        <div className="m-auto my-2 rounded-full h-10 w-10 border-t-4 border-white/50 animate-spin"></div>
+        <p className="text-xl text-center">Redirecting ...</p>
+      </div>
+    );
+  }
 }
