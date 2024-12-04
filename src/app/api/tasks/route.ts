7df5c1758f5908586ce879948a83d67baf2f4447
Glitch-guy0 <prajwalm9845@gmail.com>
2024-12-04 15:04:24 +0530

    completed v1

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,4 +1,5 @@
 import { Database } from "@/appwrite/account";
+import { ID, Permission, Role } from "appwrite";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -10,8 +11,50 @@ export async function GET(request: NextRequest) {
   );
 
   const filteredTasks = tasks?.documents.filter((task) => task.User === user);
-  return NextResponse.json({ tasks: filteredTasks});
+  return NextResponse.json({ tasks: filteredTasks });
 }
 
-
 // post method
+export async function POST(request: NextRequest) {
+  const database = Database();
+  try {
+    const user = await request.cookies.get("user")?.value;
+    const body = await request.json();
+    const task = await database?.createDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTION_ID!,
+      ID.unique(),
+      {
+        Task: body.task,
+        User: user,
+      }
+    );
+    console.log(task);
+    if (task) {
+      return NextResponse.json({ status: true });
+    } else {
+      throw new Error();
+    }
+  } catch (err) {
+    return NextResponse.json({ status: false });
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const { id } = await request.json();
+    const database = Database();
+    const status = await database?.deleteDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTION_ID!,
+      id
+    );
+    if (await status) {
+      return NextResponse.json({ status: true });
+    } else {
+      throw new Error();
+    }
+  } catch (_err) {
+    return NextResponse.json({ status: false });
+  }
+}
--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -3,44 +3,71 @@
 import { IoIosAddCircle } from "react-icons/io";
 import { FaTrash } from "react-icons/fa6";
 import { useEffect, useState } from "react";
-import { Database } from "@/appwrite/account";
 import axios from "axios";
 
 export default function todoPage() {
-
-
   const [tasks, setTasks] = useState<any>(null);
 
   useEffect(() => {
-    (async function(){
+    (async function () {
       const tasks = await axios.get("/api/tasks");
       setTasks(tasks.data.tasks);
-    })()
-  }, []);
+    })();
+  },[tasks]);
 
+
+  async function deleteHandler(e: { preventDefault: () => void }, id: string) {
+    e.preventDefault();
+    try {
+      const { data } = await axios.delete("/api/tasks/", { data: { id } });
+      if (data.status) {
+        // setTasks(data.tasks);
+      }
+    } catch (error) {
+      console.log("something went wrong");
+    }
+  }
 
   return (
     <div className="w-full max-w-[800px] mx-auto relative h-svh flex flex-col">
       {/* same file */}
       <InputTask />
       <div className=" h-full flex flex-col gap-4 mt-4 mx-2 ">
-
-        {tasks && tasks.map((task: { $id: string; Task: string;}) => (
-          <div className="bg-zinc-700/20 rounded-lg flex overflow-hidden" key={task.$id}>
-            <div className="px-4 py-2 flex-grow">
-              <h1 className="text-xl text-white/90">{task.Task}</h1>
+        {tasks &&
+          tasks.map((task: { $id: string; Task: string }) => (
+            <div
+              className="bg-zinc-700/20 rounded-lg flex overflow-hidden"
+              key={task.$id}
+            >
+              <div className="px-4 py-2 flex-grow">
+                <h1 className="text-xl text-white/90">{task.Task}</h1>
+              </div>
+              <button className="h-full bg-rose-700 w-[15%] sm:w-[5%] flex items-center justify-center" onClick={(e) => deleteHandler(e,task.$id)}>
+                <FaTrash />
+              </button>
             </div>
-            <button className="h-full bg-rose-700 w-[15%] sm:w-[5%] flex items-center justify-center">
-              <FaTrash />
-            </button>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
 }
 
 function InputTask() {
+  const [task, setTask] = useState("");
+
+  async function submitHandler(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/tasks", { task });
+      if (res.data.status) {
+        setTask("");
+      } else {
+        throw new Error();
+      }
+    } catch (_err) {
+      setTask("something went wrong try again");
+    }
+  }
   return (
     <>
       {/* user input */}
@@ -51,9 +78,15 @@ function InputTask() {
             type="text"
             placeholder="Add Task"
             className="flex-grow m-2 bg-transparent outline-none px-3 py-1"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
           />
-          <button className="">
-            <IoIosAddCircle className="text-[40px] text-zinc-700" />
+          <button onClick={(e) => submitHandler(e)}>
+            <IoIosAddCircle
+              className={`text-[40px] text-zinc-700 ${
+                task ? "text-sky-700" : ""
+              }`}
+            />
           </button>
         </form>
       </div>
